6bf80a699e9d191ba291236e8345863067ad7fb1
Donald <donald.nguyen.it@gmail.com>
2023-09-05 09:53:45 +0700

    change code

---
+++ src/Enumerators/PermissionName.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Dearvn\FilamentAccessControl\Enumerators;
+
+class PermissionName
+{
+    /** @var string */
+    public const UPDATE_ADMIN_USERS = 'admin-users.update';
+
+    /** @var string */
+    public const UPDATE_PERMISSIONS = 'permissions.update';
+}
